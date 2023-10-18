286d6d970b0399fc7db002011db7ee80a289ca40
cijiugechu <cijiugechu@gmail.com>
2023-10-18 20:16:11 +0800

    chore: make `dmzj-proto` work with `crates.io`

--- crates/dmzj-proto/build.rs
+++ crates/dmzj-proto/build.rs
@@ -1,14 +1,17 @@
 extern crate protobuf_codegen;
 
 fn main() {
-    let customize = protobuf_codegen::Customize::default().tokio_bytes(true);
+    if std::env::var_os("GENERATE_PROTOBUF").is_some() {
+        let customize =
+            protobuf_codegen::Customize::default().tokio_bytes(true);
 
-    protobuf_codegen::Codegen::new()
-        .customize(customize)
-        .pure()
-        .out_dir("src/protos")
-        .inputs(["protos/comic.proto"])
-        .include("protos")
-        .run()
-        .expect("Codegen failed.");
+        protobuf_codegen::Codegen::new()
+            .customize(customize)
+            .pure()
+            .out_dir("src/protos")
+            .inputs(["protos/comic.proto"])
+            .include("protos")
+            .run()
+            .expect("Codegen failed.");
+    }
 }
